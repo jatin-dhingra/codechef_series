079b0d71b081ba6a3eeca052207bcfa0415e2fda
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2021-06-02 20:17:42 +0530

    Create codechef_maylunchtime_TANJ1.cpp

---
+++ codechef_maylunchtime_tandj1#include <bits/codechef_maylunchtime_TANJ1.cpp
@@ -0,0 +1,35 @@
+
+//THIS CODE BELONGS TO JATIN DHINGRA
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int a,b,c,d,k;
+    cin>>a>>b>>c>>d>>k;
+    int one=abs(a-c);
+    int two=abs(b-d);
+    int sum=one+two;
+    if(sum>k)
+    {
+      cout<<"NO"<<endl;
+    }
+    else if(sum==k)
+    {
+      cout<<"YES"<<endl;
+    }
+    else if((k-sum)%2==0)
+    {
+      cout<<"YES"<<endl;
+    }
+    else
+    {
+      cout<<"NO"<<endl;
+    }
+
+  }
+}
