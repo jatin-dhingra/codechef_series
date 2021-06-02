74e3b4198064266b0c2341268863738707a9802f
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2021-06-02 20:15:40 +0530

    Create codechef_maylunchtime_hoops.cpp

---
+++ codechef_maylunchtime_hoops.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    if(n==1){
+      cout<<1<<endl;
+    }
+    else
+    {
+      cout<<ceil(n/2)+1<<endl;
+    }
+  }
+}
